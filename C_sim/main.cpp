e90b61ed98385b40070c654d16317d56eb7ad503
李松宇 <lisongyuandy@foxmail.com>
2025-01-01 10:44:25 +0800

    Initial Commit

---
+++ C_sim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C_sim
+//
+//  Created by songyu li on 2025/1/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
